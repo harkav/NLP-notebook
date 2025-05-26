c5c115a61231c082459ecf65088721707539d520
Harald <harald_kavli@hotmail.com>
2025-05-26 21:20:10 +0200

    write doc chunker, continue working on vector builder

--- vector_builder.py
+++ vector_builder.py
@@ -1,3 +1,4 @@
+import sys
 from tf_idf import pre_process
 import pathlib
 """
@@ -6,7 +7,7 @@ Project for creating vectors out of a set of document
 """
 
 
-def find_all_unique_words_in_folder(dir_path: str) -> set[str]: 
+def build_word_list_from_input_and_corpus(dir_path: str, input_doc : str) -> list[str]: 
     """
     """
     
@@ -16,12 +17,31 @@ def find_all_unique_words_in_folder(dir_path: str) -> set[str]:
     
     for maybe_file in dirpath.iterdir():
         if pathlib.Path.is_file(maybe_file): 
-            file_as_str = get_content_as_string(maybe_file)
-            tokens = pre_process(file_as_str)
-            tokens_lower = (token.lower() for token in tokens) # is this a proper set comprehension? 
-            for e in tokens_lower: 
-                all_words.add(e)
-    return all_words
+            get_unique_words_in_a_doc(all_words, maybe_file)
+    try: 
+        input_path = pathlib.Path(input_doc)
+        get_unique_words_in_a_doc(all_words, input_path)
+    except (FileNotFoundError, IOError): 
+        sys.exit("Could not read input doc")
+    
+    return list(sorted(all_words))
+
+def get_unique_words_in_a_doc(all_words: set[str], maybe_file: pathlib.Path):
+    """
+    Extracts all unique words from a document. Mutates the set of all words in corpus + input.
+    
+    Args: 
+        all_words (set[str]): all words found thus far.
+        maybe_file (pathlib.Path): A path.
+    
+    
+    
+    """
+    
+    file_as_str = get_content_as_string(maybe_file)
+    tokens = pre_process(file_as_str)
+    tokens_lower = {token.lower() for token in tokens} # is this a proper set comprehension? 
+    all_words.update(tokens_lower)
             
             
             
@@ -34,7 +54,7 @@ def get_content_as_string(filename : pathlib.Path) -> str:
         return file_as_str
          
         
-print(find_all_unique_words_in_folder("./test-docs"))
+print(build_word_list_from_input_and_corpus("./test-docs", "test-input.txt"))
 
 
 """
@@ -53,5 +73,8 @@ Compare: Compute cosine similarity between every pair of document vectors.
 Detect: Flag pairs with similarity over a chosen threshold.
 
 Output: Present flagged pairs and similarity scores.
+
+
+What counts as a document? Several options, try to go for a gliding window. 
 
 """
