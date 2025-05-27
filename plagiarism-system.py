71256324495c520b62b8cef6ef0640c069cdd588
Harald <harald_kavli@hotmail.com>
2025-05-27 06:49:47 +0200

    start moving and adapting part of the tf-idf funcs into the system

--- plagiarism-system.py
+++ plagiarism-system.py
@@ -10,15 +10,15 @@ class Plagiarism_System:
         self._input_file = input_file
         self._document_dir = document_dir
         self._vocab = [] # will contain all the unique terms
-        self._word_counts = dict()
+        self._doc_frequency_dict = dict()
         # mapping.... Or mappings.
         self._doc_chunks_input = [] # might be a good idea to keep these separate. 
         self._doc_chunks = []
-        self.preprocess(self, self._input_file, self._document_dir)
+        self.preprocess_documents(self, self._input_file, self._document_dir)
         
         
         
-    def preprocess(self, input_file, document_dir): 
+    def preprocess_documents(self, input_file, document_dir): 
         pass 
     # preprocess the docs, fill out the constructor. 
     
@@ -41,104 +41,89 @@ class Plagiarism_System:
 
 # Move tf-idf files into this class, since we're probably going to modify it slightly. Perhaps modify, move to ./utils/tf-idf along with cosine sim. 
 
-def pre_process(document : str) -> list[str]:
-    """
-    Separates words from puncts.
-    
-    Args: 
-        document (str): the document.
+    def regex_find_all_words(self, document : str) -> list[str]:
+        """
+        Separates words from puncts.
         
-    Returns: 
-        document_as_list (list[str]): the document as a list 
-    
-    """ 
-    
-    return re.findall(r"\b\w+\b", document)
-
-
-
-def term_frequency(term : str, document: str) -> int:
-    """
-    Find term frequency in a document. 
-    
-    Args: 
-        term (str): the term in question.
-        document (str): the document, represented as a str.
-        
-    Returns:
-        frequency (int): the count of term in document.
-
-    """ 
-    words_in_doc = [word.lower() for word in pre_process(document)]
-    count = 0
-    
-    for word in words_in_doc: 
-        if word == term.lower():
-            count += 1
-    return count
-    
-    
-    
-
-
-def document_frequency(term: str, documents: list[str]) -> int: 
-    """
-    Find the num of docs that contains the term. 
-    
-    Args: 
-        term (str): the term.
-        documents (list[str]): the documents of the corpus.
-        
-    Returns: 
-        Frequency (int): the num of occurrences of term in the documents.
-    
-    """
-    count = 0
-    for doc in documents: 
-        doc_split = [word.lower() for word in pre_process(doc)]
-        if term.lower() in doc_split: 
-            count += 1
+        Args: 
+            document (str): the document.
             
-    return count
-
-#TODO maybe consider doing with the doc frequency during the creation of the chuncks. 
-
-
-def inverse_document_frequency(term: str, documents: list[str]) -> float: 
-    """
-    Finds the inverse document frequency of the term.
-    
-    Args: 
-        term (str): the term.
-        documents (list[str]): the documents.
+        Returns: 
+            document_as_list (list[str]): the document as a list 
         
-    Returns:
-        The inverse document frequency of the term.
-
-    
-    """
-    
-    N = len(documents)
-    df = document_frequency(term, documents)
-    
-    
-    return math.log(N / (1 + df))
-
-
-def tf_idf(term: str, single_document: list[str], documents: list[str]) -> float: 
-     
-    #
-    """
-    Returns term frequency * inverse document frequency.
-    
-    Args: 
-        term (str): the term you're trying to find the tf-idf for.
-        single_document (str): the document you're trying to find the tf-idf for.
-        documents (list[str]): the whole corpus.
+        """ 
         
-    Returns: 
-        tf-idf (float): the tf-idf based on the input.
-    """
-    return term_frequency(term, single_document) * inverse_document_frequency(term, documents)
+        return re.findall(r"\b\w+\b", document)
+
+
+
+    def term_frequency(self, term : str, document: str) -> int:
+        """
+        Find term frequency in a document. 
+        
+        Args: 
+            term (str): the term in question.
+            document (str): the document, represented as a str.
+            
+        Returns:
+            frequency (int): the count of term in document.
+
+        """ 
+        words_in_doc = [word.lower() for word in self.regex_find_all_words(document)]
+        count = 0
+        
+        for word in words_in_doc: 
+            if word == term.lower():
+                count += 1
+        return count
+        
+        
+        
+
+
+    def document_frequency(self) -> int: 
+      
+       
+        return self._doc_frequency_dict["term"]
+
+    #TODO maybe consider doing with the doc frequency during the creation of the chuncks. 
+
+
+    def inverse_document_frequency(self, term: str, documents: list[str]) -> float: 
+        """
+        Finds the inverse document frequency of the term.
+        
+        Args: 
+            term (str): the term.
+            documents (list[str]): the documents.
+            
+        Returns:
+            The inverse document frequency of the term.
+
+        
+        """
+        
+        N = len(documents)
+        df = self.document_frequency(term, documents)
+        
+        
+        return math.log(N / (1 + df))
+
+
+    def tf_idf(self, term: str, single_document: list[str], documents: list[str]) -> float: 
+        
+        #
+        """
+        Returns term frequency * inverse document frequency.
+        
+        Args: 
+            term (str): the term you're trying to find the tf-idf for.
+            single_document (str): the document you're trying to find the tf-idf for.
+            documents (list[str]): the whole corpus.
+            
+        Returns: 
+            tf-idf (float): the tf-idf based on the input.
+        """
+        return self.term_frequency(term, single_document) * self.inverse_document_frequency(term, documents)
 
 
