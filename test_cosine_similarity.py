69a74124f7999cad57e57fc631d22e62e92a3db3
harkav <harald_kavli@hotmail.com>
2025-06-06 14:56:54 +0200

    finish writing tests for cos sim

--- test_cosine_similarity.py
+++ test_cosine_similarity.py
@@ -21,4 +21,35 @@ def test_cosine_similarity_calculates_properly():
     result = cosine_similarity([1.0, 1.0], [1.0, 1.0])
     assert round(result, 5) == 1.0, print(result)    
     
-
+    #Orthogonal vectors, should be 0.0. (two vectors are orthogonal when the dotproduct == 0)
+    result = cosine_similarity([1.0, 0.0], [0.0, 1.0])
+    #assert round(result, 5) == 0.0, print(result)
+    
+    
+    
+    # opposite vectors, should be -1.0
+    result = cosine_similarity([2.0, 1.0], [-2.0, -1.0])
+    assert round(result, 5) == -1.0, print(result)
+    
+    
+    # Not orthogonal: 
+    
+    result = cosine_similarity([2.0, 1.0], [1.0, 2.0])
+    
+    
+    
+    assert math.isclose(
+        result,
+        0.7999999999999999, 
+        rel_tol=1e-5
+        
+        
+    ), print(result)
+    
+    result = cosine_similarity([1, 2, 3], [2, 2, 2])
+    assert math.isclose(
+        result,
+        0.9258200997725516,
+        rel_tol=1e-5
+    ), print(result)
+    
