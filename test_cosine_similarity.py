3c8fabb78f5c6bafe078725f0a99eb897e641e9c
harkav <harald_kavli@hotmail.com>
2025-06-06 14:28:19 +0200

    begin working on cosine similarity tests

---
+++ test_cosine_similarity.py
@@ -0,0 +1,11 @@
+import pytest 
+import math
+from cosine_similarity import cosine_similarity
+
+
+def test_validate_input(): 
+    pass 
+
+def test_cosine_similarity_calculates_properly(): 
+    pass 
+
